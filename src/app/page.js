3dd753c80735e37dc40efa8473345dd3631ed3f7
Zubair Khan <zubairkhan8195@gmail.com>
2024-03-13 07:43:33 +0500

    header some changing and header complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,6 @@
 import Link from "next/link";
+import Header from "./components/layout/Header";
 
 export default function Home() {
-  return (
-    <header className="flex items-center justify-between">
-      <Link href="" className="text-primary font-semibold text-2xl">
-        ST PIZZA
-      </Link>
-      <nav className="flex items-center gap-8 text-gray-500 font-semibold">
-        <Link href="">Home</Link>
-        <Link href="">Menu</Link>
-        <Link href="">About</Link>
-        <Link href="">Contact</Link>
-        <Link href="" className="bg-primary rounded-full text-white px-8 py-2">
-          Login
-        </Link>
-      </nav>
-    </header>
-  );
+  return <Header />;
 }
