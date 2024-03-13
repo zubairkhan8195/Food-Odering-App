4d3d8c40b5f30bb5b596cab9593868977147a728
Zubair Khan <zubairkhan8195@gmail.com>
2024-03-13 07:31:53 +0500

    header complete

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={roboto.className}>
-        <main className="max-w-6xl mx-auto border">{children}</main>
+        <main className="max-w-6xl mx-auto p-4">{children}</main>
       </body>
     </html>
   );
